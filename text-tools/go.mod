fbf58be597c42de4b39799c09029f8fa2a4dacb6
Eldio Santos Junior <eldiosantos@gmail.com>
2020-02-25 21:41:56 +0000

    Added some tests

---
+++ text-tools/go.mod
@@ -0,0 +1,5 @@
+module github.com/Eldius/text-tools
+
+go 1.13
+
+require github.com/Code-Hex/Neo-cowsay v1.0.1
---
+++ text-tools/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay"
+)
+
+var message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+func main() {
+	say, err := cowsay.Say(
+		cowsay.Phrase(message),
+		cowsay.Type("default"),
+		cowsay.BallonWidth(40),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(say)
+}
