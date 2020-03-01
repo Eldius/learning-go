81fbc6200f292f1f5e465a9b26c33b3c1afad092
Eldio Santos Junior <eldiosantos@gmail.com>
2020-03-01 19:40:08 +0000

    Improved the termui display code to support window resizing

--- terminal-gui-tests/tools/termui/termui.go
+++ terminal-gui-tests/tools/termui/termui.go
@@ -15,30 +15,23 @@ const (
 	mainPanelHeight = 30
 	mainPanelWidth  = 130
 	displayHeight   = 5
-	maxTweets       = 30
+	maxTweets       = 100
 )
-
-func getTopLeft(i int) (int, int) {
-	return 1, 1 + i*displayHeight
-}
-
-func getBottomRight(i int, maxWidh int) (int, int) {
-	return maxWidh, 5 + i*displayHeight
-}
 
 var currentPosition int = 0
 var statuses []twitter.Tweet
-
+var termWidth, termHeight int
 /*
 Main tests gizak/termui
 */
 func Main() {
+
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 
-	termWidth, termHeight := ui.TerminalDimensions()
+	termWidth, termHeight = ui.TerminalDimensions()
 
 	p := widgets.NewParagraph()
 	p.Title = "Loading..."
@@ -46,6 +39,7 @@ func Main() {
 	p.SetRect(5, 5, termWidth, termHeight)
 
 	ui.Render(p)
+
 	refreshTweets()
 	showTweets()
 
@@ -71,7 +65,7 @@ func refreshTweets() {
 }
 
 func scrollDown() {
-	if currentPosition < len(statuses) {
+	if currentPosition < len(statuses) - 1 {
 		currentPosition = currentPosition + 1
 		showTweets()
 	}
@@ -85,17 +79,33 @@ func scrollUp() {
 }
 
 func showTweets() {
-	termWidth, termHeight := ui.TerminalDimensions()
 	ui.Clear()
+	termWidth, termHeight = ui.TerminalDimensions()
 	for i, s := range statuses[currentPosition:] {
-		displayTweet(i, s, termHeight, termWidth)
+		displayTweet(i, s)
 	}
 }
 
-func displayTweet(i int, s twitter.Tweet, maxHeight int, maxWidh int) {
+func getMaxX() int {
+	return termWidth - 2
+}
+
+func getMaxY() int {
+	return termHeight - 2
+}
+
+func getTopLeft(i int) (int, int) {
+	return 1, 1 + i*displayHeight
+}
+
+func getBottomRight(i int) (int, int) {
+	return getMaxX(), 5 + i*displayHeight
+}
+
+func displayTweet(i int, s twitter.Tweet) {
 	x0, y0 := getTopLeft(i)
-	x1, y1 := getBottomRight(i, maxWidh)
-	if y1 > maxHeight {
+	x1, y1 := getBottomRight(i)
+	if y1 > getMaxY() {
 		return
 	}
 	p := widgets.NewParagraph()
